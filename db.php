506a4e8ffe08ba2674dd81a534ba4548f8e6d2cf
mackliu <macklun@gmail.com>
2024-05-30 10:53:39 +0800

    使用global 來引用全域變數進function使用

---
+++ db.php
@@ -0,0 +1,35 @@
+<?php
+
+$dsn = "mysql:host=localhost;charset=utf8;dbname=school";
+$pdo = new PDO($dsn, 'root', '');
+
+function all()
+{
+    global $pdo;
+    $sql = "SELECT * FROM `dept`";
+    $rows = $pdo->query($sql)->fetchAll(PDO::FETCH_ASSOC);
+
+    return $rows;
+}
+
+function find($id)
+{
+    global $pdo;
+    $sql = "SELECT * FROM `dept` WHERE `id`='{$id}'";
+    $row = $pdo->query($sql)->fetch(PDO::FETCH_ASSOC);
+
+    return $row;
+}
+
+
+/**
+ * 在頁面上快速顯示陣列內容
+ * direct dump
+ * @param $array 輸入的參數需為陣列
+ */
+function dd($array)
+{
+    echo "<pre>";
+    print_r($array);
+    echo "</pre>";
+}
--- index.php
+++ index.php
@@ -1,27 +1,23 @@
-<style>
-    * {
-        font-family: 'Courier New', Courier, monospace;
-    }
-</style>
-<h1>自訂函式</h1>
 <?php
-include_once "libray.php";
-
-$a = ['A', 'B', 'C', 'D', 'E'];
-$b = [
-    '姓名' => '劉勤永',
-    '學號' => '103001',
-    '數學' => '90',
-    '國文' => 67,
-    '英文' => 56
-];
-
-//dd($a);
-//dd($b);
-
-stars();
-stars('菱形', 11);
-stars('矩形', 7);
-
+include_once "db.php";
 
 ?>
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>自訂函式</title>
+</head>
+
+<body>
+    <?php
+
+    dd(all());
+    dd(find(3));
+
+    ?>
+</body>
+
+</html>
